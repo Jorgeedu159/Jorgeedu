cdba7405de7ee47cd8b7a4a549e67db6e6018a62
Jorge Eduardo Silva <jorgerdu1590@gmail.com>
2023-08-31 17:42:58 -0300

    Conitinuação
    i'm tired

--- ExplorerLab/src/main.js
+++ ExplorerLab/src/main.js
@@ -1,1 +1,20 @@
 import "./css/index.css"
+
+const ccBgColor01 = document.querySelector(".cc-bg svg > g g:nth-child(1) path")
+
+const ccBgColor02 = document.querySelector(".cc-bg svg > g g:nth-child(2) path")
+
+
+function setCardType(type){
+  const color = {
+    visa: ["#436d99", "2D57F2"],
+    mastercard: ["#df6f29", "#c69347"],
+    default: ["black", "gray"],
+  }
+
+  ccBgColor01.setAttribute("fill", colors[type][0])
+  ccBgColor02.setAttribute("fill", colors[type][1])
+
+}
+
+setCardType("mastercard")
